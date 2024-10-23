dea28b85b1540bee3865c25d299625260788a829
Nirmohi <76029713+Vedantjn@users.noreply.github.com>
2024-10-23 17:54:31 +0530

    first commit

---
+++ 1.c
@@ -0,0 +1,36 @@
+// #include <stdio.h>
+
+// int main() {
+//     int num = -25;
+
+//     printf("Formatted with width 5: %+5d\n", num);  
+//     printf("Formatted with width 5 (positive): %+5d\n", 25); 
+
+//     return 0;
+// }
+
+
+// Online C compiler to run C program online
+
+// #include <stdio.h>
+
+// int main() {
+//     char str[100];
+
+//     printf("Enter a string: ");
+    
+//     // scanf_s("%s", str, (unsigned)_countof(str));
+    
+//     fgets(str, sizeof(str), stdin);
+
+
+//     printf("You entered: %s\n", str);
+                
+//     return 0;
+// }
+
+#include<stdio.h>
+
+int main() {
+    printf("%10.4f", 4.2);
+}
