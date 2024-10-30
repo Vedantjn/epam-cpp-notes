c7112bd4a798227b04cd4efb17de5c7294fab256
Vedant Jain <vedant_jain@epam.com>
2024-10-30 07:06:41 +0530

    When we've already defined constructor, then default constructor won't be called

---
+++ 29 October 2024/practice1.cpp
@@ -0,0 +1,33 @@
+// When we've already defined constructor, then default constructor won't be called
+
+#include <iostream>
+using namespace std;
+
+class Student {
+    int roll;
+    string name;
+public:
+    Student(int r, string n) {
+        roll = r;
+        name = n;
+        cout << "Parameterized constructor called" << endl;
+    }
+    
+    void display() {
+        cout << "Roll: " << roll << ", Name: " << name << endl;
+    }
+};
+
+int main() {
+    // This will call parameterized constructor
+    Student s1(101, "John");
+    s1.display();
+    
+    // This will give error as default constructor is not available
+    // Student s2; // Uncommenting this line will cause compilation error
+    
+    return 0;
+}
+
+// Parameterized constructor called
+// Roll: 101, Name: John
