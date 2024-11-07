e682f9214f759f45ea7ab366de74d4d8f275b6ca
Vedant Jain <vedant_jain@epam.com>
2024-11-07 13:19:33 +0530

    base class pointe, derived class object

---
+++ 7 November 2024/base_class_pointer_derived_class_object.cpp
@@ -0,0 +1,49 @@
+// base class pointer, derived class object
+
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    void draw() { cout << "Drawing shape\n"; }
+};
+
+class Circle : public Shape {
+public:
+    void draw() { cout << "Drawing circle\n"; }
+};
+
+int main() {
+    Shape* shape = new Circle();
+    shape->draw();  // Calls Shape::draw() without runtime polymorphism
+    
+    delete shape;
+    return 0;
+}
+
+// Drawing shape
+
+// ------------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void draw() { cout << "Drawing shape\n"; }
+};
+
+class Circle : public Shape {
+public:
+    void draw() override { cout << "Drawing circle\n"; }
+};
+
+int main() {
+    Shape* shape = new Circle();
+    shape->draw();  // Calls Shape::draw() without runtime polymorphism
+    
+    delete shape;
+    return 0;
+}
+
+// Drawing circle
