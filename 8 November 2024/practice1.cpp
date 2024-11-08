2bd41770230d57c3072267d36b18aadfe602dc70
Vedant Jain <vedant_jain@epam.com>
2024-11-08 13:20:22 +0530

    bad alloc

--- 8 November 2024/practice1.cpp
+++ 8 November 2024/practice1.cpp
@@ -9,7 +9,10 @@ public:
     }
     
     void memFunc() {
-        int* ptr = new int[100000000000]; // terminate called after throwing an instance of 'std::bad_alloc'
+        // terminate called after throwing an instance of 'std::bad_alloc'
+        // what():  std::bad_alloc
+        int* ptr = new int[100000000000];
+        int* ptr2 = (int*)malloc(sizeof(int) * 100000000000); 
     }
     
     int memFunc2(int x, int y) { // y as 0 exception handling
