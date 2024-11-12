7a43d842632d9cf82973485454d000354e293fc5
Vedant Jain <vedant_jain@epam.com>
2024-11-12 20:19:03 +0530

    stack unwinding

---
+++ 12 November 2024/stack_unwinding.cpp
@@ -0,0 +1,36 @@
+// in stack unwinding, before reaching the catch block, all the fully constructed objects will be destructed
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    A() { cout << "A constructed\n"; }
+    ~A() { cout << "A destructed\n"; }
+};
+
+class B {
+public:
+    B() { cout << "B constructed\n"; }
+    ~B() { cout << "B destructed\n"; }
+};
+
+void func() {
+    A a;
+    B b;
+    throw runtime_error("Exception thrown in func()");
+}
+
+int main() {
+    try {
+        func();
+    } catch (const runtime_error& e) {
+        cout << "Caught exception: " << e.what() << endl;
+    }
+    return 0;
+}
+
+// A constructed
+// B constructed
+// B destructed
+// A destructed
+// Caught exception: Exception thrown in func()
