f4326998c40a6f665ee8773262e5c23927b2fac5
Vedant Jain <vedant_jain@epam.com>
2024-11-12 15:09:14 +0530

    code updated

--- 12 November 2024/files1.cpp
+++ 12 November 2024/files1.cpp
@@ -1,17 +1,25 @@
+// This program demonstrates redirecting cout to write to a file
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main() {
-    ofstream outFile("output.txt");
+    // Create an output file stream
+    ofstream outFile("outputFromFiles1.txt");
+    
+    // Save the original cout buffer
     streambuf *original = cout.rdbuf();
+    
+    // Redirect cout to write to the file
     cout.rdbuf(outFile.rdbuf());
 
-    // Your output statements would go here
+    // Any cout statements here will write to the file instead of console
     cout << "This text will be written to the file" << endl;
 
-    // restore
+    // Restore cout to write to console again
     cout.rdbuf(original);
+    
+    // Close the file
     outFile.close();
 
     return 0;
