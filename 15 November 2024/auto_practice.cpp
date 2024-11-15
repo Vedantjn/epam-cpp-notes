f02039643ac3537bd0f702d2698e350c5d655e78
Vedant Jain <vedant_jain@epam.com>
2024-11-15 15:25:41 +0530

    code updated

--- 15 November 2024/auto_practice.cpp
+++ 15 November 2024/auto_practice.cpp
@@ -2,9 +2,28 @@
 #include<vector>
 using namespace std;
 
+template<typename T>
+int sum(T val) {
+    return val;
+}
+
+template<typename T, typename... Args>
+int sum(T x, Args... args) {
+    return x + sum(args...);
+}
+
+
+int difference(auto val1, int val2) { // only after c++ 20
+    cout << val2 - val1 << endl;
+}
+
 int main(){
+    difference(3, 5);
+
     vector<int>vec = {1, 4, 2, 55, 2};
-    
+
+    int ans = sum(1, 2, 3, 4, 5);
+    cout << ans << endl;
     // Method 1: Traditional for loop with index
     for(int i = 0; i < vec.size(); i++) {
         cout << vec[i] << endl;
