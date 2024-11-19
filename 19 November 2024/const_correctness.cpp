ba44cb105ea49e032eb9e49cea780eb686d3025a
Vedant Jain <vedant_jain@epam.com>
2024-11-19 20:03:01 +0530

    const correctness

---
+++ 19 November 2024/const_correctness.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class MyClass {
+public:
+    int value = 0;
+
+    void setValue(int val) { // Non-const member function
+        value = val;
+    }
+
+    int getValue() const { // Const member function
+        return value; 
+    }
+};
+
+int main() {
+    const MyClass constObj;    // Const object
+    MyClass nonConstObj;       // Non-const object
+
+    nonConstObj.setValue(10);  // Valid, modifies value
+    cout << "Non-const object's value: " << nonConstObj.getValue() << std::endl;
+
+    // constObj.setValue(10);  // Error, const object cannot call non-const function
+    cout << "Const object's value: " << constObj.getValue() << std::endl; // Valid, const object can call const function
+
+    return 0;
+}
