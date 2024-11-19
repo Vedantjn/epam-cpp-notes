68153ce71814f5c9820e54d828ca64760e758f5d
Vedant Jain <vedant_jain@epam.com>
2024-11-19 12:13:03 +0530

    unique pointer

---
+++ 19 November 2024/unique_pointer.cpp
@@ -0,0 +1,29 @@
+// 1. std::unique_ptr
+//    - Smart pointer in C++ providing exclusive ownership of dynamically allocated resources
+//    - Ensures exactly one owner of the resource at any time
+
+// 2. Key Features:
+//    - Exclusive Ownership: Only one std::unique_ptr can own the resource
+//    - Non-Copyable: Cannot be copied but can be moved using std::move
+//    - Automatic Deletion: Automatically deletes managed object when going out of scope
+
+#include<iostream>
+#include<memory>
+using namespace std;
+
+int main(){
+    
+    unique_ptr<int> uptr(new int(43));
+    cout << "Value : " << *uptr << endl;
+
+    unique_ptr<int> uptr2 = move(uptr);
+
+    cout << "Value uptr2 move: " << *uptr2 << endl;
+    if(!uptr) {
+        cout << "uptr is now null after transfet" << endl;
+    }
+
+    cout << "Value after transfer: " << *uptr2 << endl;
+
+    return 0;
+}
