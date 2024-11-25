ca67b46216d5c67463367c7fe9df9e7bdb18b5c0
Vedant Jain <vedant_jain@epam.com>
2024-11-25 15:09:09 +0530

    custom deleter practice

--- 25 November 2024/custom_deleter_practice.cpp
+++ 25 November 2024/custom_deleter_practice.cpp
@@ -4,11 +4,53 @@ using namespace std;
 
 class MyClass {
 public:
-    MyClass() {cout << "MyClass constructor called" << endl'}
-    ~MyClass() {cout << "MyClass destructor called" << endl'}
+    MyClass() {cout << "MyClass constructor called" << endl; }
+    ~MyClass() {cout << "MyClass destructor called" << endl; }
+
 };
 
+void customDelete(MyClass* ptr) {
+    cout << "Custom delete called" << endl;
+    delete ptr;
+}
+
 int main(){
+    unique_ptr<MyClass, decltype(&customDelete)> ptr(new MyClass(), customDelete);
     
     return 0;
 }
+
+// MyClass constructor called
+// Custom delete called
+// MyClass destructor called
+
+// -------------------------------------------
+
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class MyClass {
+public:
+    MyClass() { cout << "MyClass constructor called\n"; }
+    ~MyClass() { cout << "MyClass destructor called\n"; }
+};
+
+void customDelete(MyClass* ptr) {
+    cout << "Custom delete called\n";
+    delete ptr;  // Deleting the object in a custom way
+}
+
+int main() {
+    shared_ptr<MyClass> ptr1(new MyClass(), customDelete);
+    {
+        shared_ptr<MyClass> ptr2 = ptr1;  // Sharing ownership
+    }  // ptr2 goes out of scope, but ptr1 still holds the object
+
+    // When ptr1 goes out of scope, the custom deleter will be invoked
+    return 0;
+}
+
+// MyClass constructor called
+// Custom delete called
+// MyClass destructor called
