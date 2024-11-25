b32281f639583a305640774f6c690ff927621cba
Vedant Jain <vedant_jain@epam.com>
2024-11-25 14:36:53 +0530

    renamed

---
+++ 25 November 2024/custom_deleter_practice.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<memory>
+using namespace std;
+
+class MyClass {
+public:
+    MyClass() {cout << "MyClass constructor called" << endl'}
+    ~MyClass() {cout << "MyClass destructor called" << endl'}
+};
+
+int main(){
+    
+    return 0;
+}
