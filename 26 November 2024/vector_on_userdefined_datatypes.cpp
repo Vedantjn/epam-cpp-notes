f4484a6b4e18ce24a38ee0b5c3afc370cfa1eb18
Vedant Jain <vedant_jain@epam.com>
2024-11-26 12:03:24 +0530

    vector on userdefined datatypes

---
+++ 26 November 2024/vector_on_userdefined_datatypes.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Person {
+public:
+    string name;
+    int age;
+
+    Person(string n, int a) : name(n), age(a) {}
+
+    void display() const {
+        cout << "Name : " << name << " and Age : " << age << endl;
+    }
+
+};
+
+int main(){
+    vector<Person> people;
+    people.emplace_back(Person("Vedant", 22));
+    people.emplace_back(Person("Vanshika", 21));
+    people.emplace_back(Person("Meenal", 24));
+    people.emplace_back("Priyal", 23);
+    for(const auto& person : people) {
+        person.display();
+    }
+    
+    return 0;
+}
