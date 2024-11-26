e1d40d9f03be3c1e0a14f56067919d08a6f15621
Vedant Jain <vedant_jain@epam.com>
2024-11-26 13:27:01 +0530

    iterator invalidation

--- 26 November 2024/iterator_invalidation.cpp
+++ 26 November 2024/iterator_invalidation.cpp
@@ -23,3 +23,24 @@ int main() {
 // Revalidate Iterators: If an operation might invalidate iterators, get new valid iterators after the operation.
 // Avoid Storing Iterators: Minimize long-term storage of iterators when modifying containers.
 // Use Stable Containers: If you need stable iterators, consider using std::list or std::map.
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main() {
+    vector<int> vec = {1, 2, 3};
+    auto it = vec.begin(); // Points to 1
+
+    vec.push_back(4); // May reallocate
+    // Now `it` might be invalidated.
+
+    // Demonstrating iterator invalidation
+    cout << "Value before potential invalidation: " << *it << endl; // Undefined behavior if `it` is invalidated
+
+    // Revalidate the iterator after modification
+    it = vec.begin(); // Reassign `it` to a valid iterator
+    cout << "Value after revalidation: " << *it << endl; // Now it is safe to use
+
+    return 0;
+}
