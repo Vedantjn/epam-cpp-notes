7b4d7d25b6fc88c69ccb7c74731e0316321ec480
Vedant Jain <vedant_jain@epam.com>
2024-11-26 13:53:02 +0530

    smart pointer

---
+++ 26 November 2024/smart_pointer_practice.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>  // For shared pointers
+
+using namespace std;
+
+class MyClass1 {
+    int value;
+    
+public:
+    MyClass1(int val) : value(val) {}
+
+    void printvalue() const {
+        cout << "Value: " << value << endl;
+    }
+};
+
+int main() {
+    // Using shared_ptr for a single object
+    shared_ptr<MyClass1> obj = make_shared<MyClass1>(10);
+    obj->printvalue();  // Print the value of obj
+
+    // Using shared_ptr for an array of objects
+    shared_ptr<MyClass1[]> ptr = make_shared<MyClass1[]>(10);  // Array of 10 MyClass1 objects
+
+    // Since shared_ptr handles memory deallocation automatically, we don't need to call delete explicitly
+
+    return 0;
+}
